3c4642c7e8424faa16f15b35613a2bd60d00af86
rumi <maziar9170@gmail.com>
2021-08-04 21:28:22 +0430

    last  one for today

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch , BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch ,BrowserRouter as Router ,HashRouter} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Blog from './components/Pages/Blog/Blog';
@@ -10,16 +10,18 @@ import Projects from './components/Pages/Projects/Projects';
 
 function App() {
   return (
-    <React.Fragment>
-    {/* <Router> */}
+    // <React.Fragment>
+    <HashRouter basename='/'>
+    <Router>
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/Portfolio' component={Projects}/>
         <Route path='/blog' component={Blog}/>
       </Switch>
-      {/* </Router> */}
-      </React.Fragment>
+      </Router>
+      </HashRouter>
+      // </React.Fragment>
   );
 }
 
